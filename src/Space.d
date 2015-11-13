a42d0fa0a8b8b69a3b15e8faaa7ca85ec1e58d0b
Shannon <shannon@meatRay.com>
2015-11-13 15:37:04 -0600

    Abstracting away rendering process.

--- src/Space.d
+++ src/Space.d
@@ -1,5 +1,15 @@
-module Fleet.Space;
+module fleet.space;
 
+import fleet.stage;
+
+import core.thread;
+import core.time;
+
+void main()
+{
+	Stage stage =createStage();
+	Thread.sleep(dur!"seconds"(4));
+}
 class Space
 {
 }
--- src/Stage.d
+++ src/Stage.d
@@ -1,36 +1,62 @@
-module Fleet.Stage;
+module fleet.stage;
 
 import derelict.sdl2.sdl;
 import derelict.opengl3.gl3;
 
 /+debug +/import std.stdio :writefln;
 
-public static Stage Main_Stage;
-
-private static SDL_Window* _Window;
-private static SDL_GLContext _GLContext;
-static this()
+public static Stage createStage()
 {
+	import std.exception :enforce;
 	DerelictGL3.load();
 	DerelictSDL2.load();
 	
-	if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
-		{ writefln("Error initializing SDL!"); }
-	if( (_Window = SDL_CreateWindow("3.2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN)) is null )
-		{ writefln("SDL ->OpenGL display creation failed!"); }
-	if( (_GLContext =SDL_GL_CreateContext(_WINDOW)) is null )
-		{ writefln("OpenGL context creation failed!"); }
+	SDL_Window* window;
+	SDL_GLContext glContext;
+	
+	enforce( SDL_Init( SDL_INIT_EVERYTHING ) >=0, "Error initializing SDL." );
+	enforce(
+		(window =SDL_CreateWindow("meatray_fleet", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN))
+		!is null,
+		"SDL ->OpenGL display creation failed!"
+	);
+	enforce( (glContext =SDL_GL_CreateContext(window)) !is null, "OpenGL context creation failed!" );
 		
 	DerelictGL3.reload();
+	
+	return new Stage( window, glContext );
 }
 
-void main()
-{
-	writef("Running!");
-}
-
+/++ Definitions on how to Render Scene Objects ++/
 class Stage
 {
+	/+ I spent way too long debating gross camelCase or PascalCase.  Got to be consistent. +/ 
+	@property public Scene currentScene(){ return this._currentScene; }
+	public Scene setScene(Scene scene){ return this._currentScene =scene; }
+	private Scene _currentScene;
+	private static SDL_Window* _window;
+	private static SDL_GLContext _glContext;
+	public this( SDL_Window* window, SDL_GLContext glcontext )
+	{
+		_window =window;
+		_glContext =glcontext;
+	}
+	public ~this()
+	{
+		SDL_GL_DeleteContext( _glContext );
+		SDL_DestroyWindow( _window );
+		SDL_Quit(); /+ Likely does nasty things in case of multiple Stages running +/
+	}
 	public void render()
 	{}
+}
+
+/++ Contains Collections of Items to be rendered ++/
+class Scene
+{
+	/+ Stage Reference? +/
+}
+interface IRenderable  /+ Replace with a `Renderer` class with options as data objects? +/
+{
+	void render();
 }
